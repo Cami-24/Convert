8f9276af08f90f1294077858cad769b77d36cb77
Cami-24 <cdgolondrina@gmail.com>
2024-07-18 21:58:29 -0600

    arreglo de minuscula

--- conversor.js
+++ conversor.js
@@ -1,2 +1,8 @@
 document.getElementById('clear').onclick = clearForm;
 document.getElementById('convert').onclick = tempConvert;
+
+function tempConvert(){
+    var celsius = document.getElementById('celsius').value;
+    var fahrenheit = document.getElementById('fahrenheit').value;
+    var kelvin = document.getElementById('kelvin').value;
+}
