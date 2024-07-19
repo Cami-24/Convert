9442330fd38dea4741b4213c0115f312a948a840
Cami-24 <cdgolondrina@gmail.com>
2024-07-18 22:04:12 -0600

    condicion de que sea un valor numerico

--- conversor.js
+++ conversor.js
@@ -5,4 +5,9 @@ function tempConvert(){
     var celsius = document.getElementById('celsius').value;
     var fahrenheit = document.getElementById('fahrenheit').value;
     var kelvin = document.getElementById('kelvin').value;
+
+    if(isNaN(celsius)|| isNaN(fahrenheit)||isNaN(kelvin)){
+        alert('Ingrese un valor numérico');
+        return
+    }
 }
